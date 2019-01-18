035750b1acaff332a0f2ffa63e4ef38ed8edc499
Kelsey Low <kklowlow@gmail.com>
2019-01-18 01:43:47 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,16 +1,16 @@
-def countdown(num)
-  while num > 0 
-    puts "#{num} SECOND(S)!"
-    num -= 1 
+def countdown(seconds)
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
   end
-  puts "HAPPY NEW YEAR!"
+  return "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(num)
-  while num > 0 
-    puts "#{num} SECOND(S)!"
-    num -= 1 
+def countdown_with_sleep(seconds)
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
     sleep(1)
   end
-  puts "HAPPY NEW YEAR!"
+  return "HAPPY NEW YEAR!"
 end
