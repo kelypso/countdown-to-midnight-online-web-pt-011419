c2cb8be0c035f3d252e9329e9b89af04e1e2afb5
Kelsey Low <kklowlow@gmail.com>
2019-01-18 01:43:07 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,3 +6,11 @@ def countdown(num)
   puts "HAPPY NEW YEAR!"
 end
 
+def countdown_with_sleep(num)
+  while num > 0 
+    puts "#{num} SECOND(S)!"
+    num -= 1 
+    sleep(1)
+  end
+  puts "HAPPY NEW YEAR!"
+end
